9e63596fed6211c15fd9703eda2632889eeda655
Waihon Yew <yewwaihon@gmail.com>
2019-12-05 23:07:44 +0800

    Edit and update articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,4 +24,15 @@ class ArticlesController < ApplicationController
     @article.destroy
     redirect_to articles_url
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
 end
