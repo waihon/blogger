d23c708420ee22da5a230fc64b655957338d8148
Waihon Yew <yewwaihon@gmail.com>
2019-12-07 09:13:34 +0800

    Allow requests to www.example.com in the development console via app object

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # To allow requests to www.example.com in the development console via app object
+  config.hosts << "www.example.com"
 end
