2be71e865deef8dac83a35018ae093029880f1fd
Waihon Yew <yewwaihon@gmail.com>
2019-12-07 15:38:43 +0800

    Create tag and tagging models, update article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments
+  has_many :taggings
+  has_many :tags, through: :taggings
 
   validates :title, presence: true
   validates :body, presence: true
