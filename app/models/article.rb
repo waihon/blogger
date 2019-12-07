0d96bd2851788d6d11bea719318b725e738dd1e7
Waihon Yew <yewwaihon@gmail.com>
2019-12-07 16:47:58 +0800

    Create an interface for tagging articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,14 @@ class Article < ApplicationRecord
 
   validates :title, presence: true
   validates :body, presence: true
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tags_string)
+    tag_names = tags_string.split(",").map { |s| s.strip.downcase }.uniq
+    new_or_found_tags = tag_names.map { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
